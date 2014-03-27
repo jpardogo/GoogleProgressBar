a0bfcf496fcf68e98b19853cd4afd6cf43a0f93b
jpardogo <jpardogo@gmail.com>
2014-03-27 01:59:25 +0000

    Refactor customView adding attribute to choose between animations (only one animation done so far)

--- library/src/main/java/com/jpardogo/android/googleprogressbar/library/GoogleProgressBar.java
+++ library/src/main/java/com/jpardogo/android/googleprogressbar/library/GoogleProgressBar.java
@@ -1,6 +1,7 @@
 package com.jpardogo.android.googleprogressbar.library;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.*;
 import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
@@ -8,24 +9,31 @@ import android.widget.ProgressBar;
 
 public class GoogleProgressBar extends ProgressBar {
     private static final int FOLDED_TRANSPARENCY = 235;
-    private static final int OPAQUE=255;
+    private static final int OPAQUE = 255;
+    private int mProgressType;
     private int mY = Integer.MAX_VALUE;
     private int mX = Integer.MAX_VALUE;
     private Paint mPaint;
     private Paint mPaint2;
     private Paint mPaint3;
-    private RectF mOval;
+    private RectF mOval = new RectF();
+    private int mDiameter;
     private Path mPath;
-    private int mHeight;
-    private int mHalfHeight;
-    private int mWidth;
-    private int mHalfWidth;
+    private int mHalf;
     private ProgressStates mCurrentState = ProgressStates.FOLDING_DOWN;
     private ProgressStates mLastState;
     private int mSpeed;
-    private int mDiameter;
     private int mUnfoldCounter = 0;
+    private int mControlPointMinimun;
+    private int mContorlPointMaximun;
 
+
+    private enum ProgressTypes {
+        CIRCLES,
+        NEXUS_CIRCLES,
+        NEXUS_CROSS,
+        NEXUS_ROATION_CROSS;
+    }
     private enum ProgressColors {
         RED("#C93437"),
         BLUE("#375BF1"),
@@ -60,6 +68,12 @@ public class GoogleProgressBar extends ProgressBar {
 
     public GoogleProgressBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,R.styleable.GoogleProgressBar,0, 0);
+        try {
+            mProgressType = a.getInt(R.styleable.GoogleProgressBar_progressType, 0);
+        } finally {
+            a.recycle();
+        }
         init(context);
     }
 
@@ -69,177 +83,173 @@ public class GoogleProgressBar extends ProgressBar {
     }
 
     private void init(Context context) {
-        mPath = new Path();
+        setIndeterminateDrawable(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
+        switch (ProgressTypes.values()[mProgressType]) {
+            case CIRCLES:
+                initCirclesProgress();
+                break;
+        }
+    }
 
+    private void initCirclesProgress() {
+        mPath = new Path();
         mPaint = new Paint();
         mPaint.setColor(Color.parseColor(ProgressColors.RED.toString()));
-
         mPaint2 = new Paint();
         mPaint2.setColor(Color.parseColor(ProgressColors.BLUE.toString()));
-
         mPaint3 = new Paint();
         mPaint3.setColor(Color.parseColor(ProgressColors.BLUE.toString()));
-        setIndeterminateDrawable(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
     }
 
     @Override
     protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        switch (ProgressTypes.values()[mProgressType]) {
+            case CIRCLES:
+                measureCircleProgress();
+                break;
+        }
+    }
+
+    private void measureCircleProgress() {
         mDiameter = Math.min(getWidth(), getHeight());
-        mHalfWidth = mDiameter / 2;
-        mHalfHeight = mDiameter / 2;
-        mWidth=mDiameter;
-        mHeight=mDiameter;
-        mOval = new RectF(0, 0, mWidth, mHeight);
-        mSpeed = mHeight/20;
+        mHalf = mDiameter / 2;
+        mOval.set(0, 0, mDiameter, mDiameter);
+        mSpeed = mDiameter / 40;
+        mControlPointMinimun = -mDiameter / 6;
+        mContorlPointMaximun = mDiameter + mDiameter / 6;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (mCurrentState == ProgressStates.FOLDING_DOWN) {
-            //Start
-            if (mY == Integer.MAX_VALUE || mY < -mHeight / 6) {
-                mY = -mHeight / 6;
-                mPaint3.setAlpha(FOLDED_TRANSPARENCY);
-            }
-
-            //Half
-            if (mY == mHeight / 2 || mY > mHeight / 2) {
-                mPaint3.setColor(Color.parseColor(ProgressColors.RED.toString()));
-                mPaint3.setAlpha(FOLDED_TRANSPARENCY);
-            }
-
-            //End
-            if (mY == mHeight + mHeight / 6 || mY > mHeight + mHeight / 6) {
-                mY = mHeight + mHeight / 6;
-                mCurrentState = ProgressStates.NOT_FOLDING;
-                mLastState = ProgressStates.FOLDING_DOWN;
-                mPaint2.setColor(mPaint3.getColor());
-                mPaint2.setAlpha(OPAQUE);
-            }
-
-            mY = mY + mSpeed;
-            canvas.drawArc(mOval, 0, -180, true, mPaint);
-            canvas.drawArc(mOval, -180, -180, true, mPaint2);
-            mPath.reset();
-            mPath.moveTo(0, mHalfHeight);
-            mPath.cubicTo(0, mY, mWidth, mY, mWidth, mHalfHeight);
-            canvas.drawPath(mPath, mPaint3);
-        } else if (mCurrentState == ProgressStates.FOLDING_LEFT) {
-
-            //Start
-            if (mX == Integer.MAX_VALUE || mX > mWidth + mWidth / 6) {
-                mX = mWidth + mWidth / 6;
-                mPaint3.setAlpha(FOLDED_TRANSPARENCY);
-            }
-
-            //Half
-            if (mX == mWidth / 2 || mX < mWidth / 2) {
-                mPaint3.setColor(Color.parseColor(ProgressColors.YELLOW.toString()));
-                mPaint3.setAlpha(FOLDED_TRANSPARENCY);
-            }
-
-            //Final
-            if (mX == 0 - mWidth / 6 || mX < 0 - mWidth / 6){
-                mX = 0 - mWidth / 6;
-                mCurrentState = ProgressStates.NOT_FOLDING;
-                mLastState = ProgressStates.FOLDING_LEFT;
-                mPaint.setColor(mPaint3.getColor());
-                mPaint.setAlpha(OPAQUE);
-            }
-
-            mX = mX - mSpeed;
-            canvas.drawArc(mOval, 90, 180, true, mPaint);
-            canvas.drawArc(mOval, -270, -180, true, mPaint2);
-            mPath.reset();
-            mPath.moveTo(mHalfWidth, 0);
-            mPath.cubicTo(mX, 0, mX, mHeight, mHalfWidth, mHeight);
-            canvas.drawPath(mPath, mPaint3);
-        } else if (mCurrentState == ProgressStates.FOLDING_UP) {
-
-            //Start
-            if (mY == mHeight + mHeight / 6 || mY > mHeight + mHeight / 6) {
-                mPaint3.setAlpha(FOLDED_TRANSPARENCY);
-            }
-
-            //Half
-            if (mY == mHeight / 2 || mY < mHeight / 2) {
-                mPaint3.setColor(Color.parseColor(ProgressColors.GREEN.toString()));
-                mPaint3.setAlpha(FOLDED_TRANSPARENCY);
-            }
-
-            //End
-            if (mY == -mHeight / 6 || mY < -mHeight / 6) {
-                mY = -mHeight / 6;
-                mCurrentState = ProgressStates.NOT_FOLDING;
-                mLastState = ProgressStates.FOLDING_UP;
-                mPaint2.setColor(mPaint3.getColor());
-                mPaint2.setAlpha(OPAQUE);
-                mPaint.setColor(mPaint3.getColor());
-                mPaint.setAlpha(OPAQUE);
-            }
-
-            mY = mY - mSpeed;
-            canvas.drawArc(mOval, 0, -180, true, mPaint);
-            canvas.drawArc(mOval, -180, -180, true, mPaint2);
-            mPath.reset();
-            mPath.moveTo(0, mHalfHeight);
-            mPath.cubicTo(0, mY, mWidth, mY, mWidth, mHalfHeight);
-            canvas.drawPath(mPath, mPaint3);
-        } else if (mCurrentState == ProgressStates.FOLDING_RIGHT) {
-
-            //Start
-            if (mX == 0 - mWidth / 6 || mX < 0 - mWidth / 6) {
-                mPaint3.setAlpha(FOLDED_TRANSPARENCY);
-            }
-
-            //Half
-            if (mX == mWidth / 2 || mX > mWidth / 2) {
-                mPaint3.setColor(Color.parseColor(ProgressColors.BLUE.toString()));
-                mPaint3.setAlpha(FOLDED_TRANSPARENCY);
-            }
-            //End
-            if (mX == mWidth + mWidth / 6 || mX > mWidth + mWidth / 6) {
-                mX = mWidth + mWidth / 6;
-                mCurrentState = ProgressStates.NOT_FOLDING;
-                mLastState = ProgressStates.FOLDING_RIGHT;
-                mPaint2.setColor(mPaint3.getColor());
-                mPaint2.setAlpha(OPAQUE);
-                mPaint.setColor(mPaint3.getColor());
-                mPaint.setAlpha(OPAQUE);
-
-            }
-
-            mX = mX + mSpeed;
-            canvas.drawArc(mOval, 90, 180, true, mPaint);
-            canvas.drawArc(mOval, -270, -180, true, mPaint2);
-            mPath.reset();
-            mPath.moveTo(mHalfWidth, 0);
-            mPath.cubicTo(mX, 0, mX, mHeight, mHalfWidth, mHeight);
-            canvas.drawPath(mPath, mPaint3);
-
-        } else if (mCurrentState == ProgressStates.NOT_FOLDING) {
-            canvas.drawArc(mOval, 90, 180, true, mPaint);
-            canvas.drawArc(mOval, -270, -180, true, mPaint2);
-
-            if (mUnfoldCounter == 10) {
-                mUnfoldCounter = 0;
-                if (mLastState == ProgressStates.FOLDING_DOWN) {
-                    mPaint2.setColor(Color.parseColor(ProgressColors.YELLOW.toString()));
-                    mCurrentState = ProgressStates.FOLDING_LEFT;
-                } else if (mLastState == ProgressStates.FOLDING_LEFT) {
-                    mPaint2.setColor(Color.parseColor(ProgressColors.GREEN.toString()));
-                    mCurrentState = ProgressStates.FOLDING_UP;
-                } else if (mLastState == ProgressStates.FOLDING_UP) {
-                    mPaint.setColor(Color.parseColor(ProgressColors.BLUE.toString()));
-                    mCurrentState = ProgressStates.FOLDING_RIGHT;
-                } else if (mLastState == ProgressStates.FOLDING_RIGHT) {
-                    mPaint.setColor(Color.parseColor(ProgressColors.RED.toString()));
-                    mCurrentState = ProgressStates.FOLDING_DOWN;
-                }
-            }
-            mUnfoldCounter++;
+        switch (ProgressTypes.values()[mProgressType]){
+            case CIRCLES:
+                makeCirclesProgress(canvas);
+                break;
+            case NEXUS_CIRCLES:
+                //http://ikslawok.free.fr/my_nexus_fr/nexus_5/bootanimation_nexus_5.gif
+                //TODO: Nexus 5 circles boot progress animation
+                break;
+            case NEXUS_CROSS:
+                //http://devfest.gdgthess.org/wp-content/uploads/2013/11/nexus-4-boot-animation.gif
+                //TODO: Galaxy nexus shinny cross boot animation
+                break;
+            case NEXUS_ROATION_CROSS:
+                //http://deathlyspectator.files.wordpress.com/2012/03/sampleb.gif
+                //TODO: Nexus one rotation cross animation
+                break;
         }
         super.onDraw(canvas);
+    }
+
+    private void makeCirclesProgress(Canvas canvas) {
+        if (mCurrentState == ProgressStates.NOT_FOLDING) {
+            notFold(canvas);
+        } else if (mCurrentState == ProgressStates.FOLDING_DOWN) {
+            mY = foldingToMaximun(mY,ProgressStates.FOLDING_DOWN,Color.parseColor(ProgressColors.RED.toString()));
+            drawYMotion(canvas);
+        } else if (mCurrentState == ProgressStates.FOLDING_RIGHT) {
+            mX = foldingToMaximun(mX,ProgressStates.FOLDING_RIGHT,Color.parseColor(ProgressColors.BLUE.toString()));
+            drawXMotion(canvas);
+        }else if (mCurrentState == ProgressStates.FOLDING_UP) {
+            mY = foldingToMinimun(mY, ProgressStates.FOLDING_UP, Color.parseColor(ProgressColors.GREEN.toString()));
+            drawYMotion(canvas);
+        } else if (mCurrentState == ProgressStates.FOLDING_LEFT) {
+            mX = foldingToMinimun(mX, ProgressStates.FOLDING_LEFT, Color.parseColor(ProgressColors.YELLOW.toString()));
+            drawXMotion(canvas);
+        }
+
+        if (mCurrentState != ProgressStates.NOT_FOLDING) {
+            canvas.drawPath(mPath, mPaint3);
+        }
+    }
+
+    private void notFold(Canvas canvas) {
+        canvas.drawArc(mOval, 90, 180, true, mPaint);
+        canvas.drawArc(mOval, -270, -180, true, mPaint2);
+        mUnfoldCounter++;
+        if (mUnfoldCounter == 10) {
+            mUnfoldCounter = 0;
+            if (mLastState == ProgressStates.FOLDING_DOWN) {
+                mPaint2.setColor(Color.parseColor(ProgressColors.YELLOW.toString()));
+                mCurrentState = ProgressStates.FOLDING_LEFT;
+            } else if (mLastState == ProgressStates.FOLDING_LEFT) {
+                mPaint2.setColor(Color.parseColor(ProgressColors.GREEN.toString()));
+                mCurrentState = ProgressStates.FOLDING_UP;
+            } else if (mLastState == ProgressStates.FOLDING_UP) {
+                mPaint.setColor(Color.parseColor(ProgressColors.BLUE.toString()));
+                mCurrentState = ProgressStates.FOLDING_RIGHT;
+            } else if (mLastState == ProgressStates.FOLDING_RIGHT) {
+                mPaint.setColor(Color.parseColor(ProgressColors.RED.toString()));
+                mCurrentState = ProgressStates.FOLDING_DOWN;
+            }
+        }
+    }
+
+    private int foldingToMaximun(int axisValue, ProgressStates lastState, int nextColor) {
+        //Start
+        if (axisValue <= mControlPointMinimun) {
+            axisValue=mControlPointMinimun;
+            mPaint3.setAlpha(FOLDED_TRANSPARENCY);
+        }
+
+        //Half
+        if (axisValue >= mHalf) {
+            mPaint3.setColor(nextColor);
+            mPaint3.setAlpha(FOLDED_TRANSPARENCY);
+        }
+        //End
+        if (axisValue >= mContorlPointMaximun) {
+            axisValue = mContorlPointMaximun;
+            mCurrentState = ProgressStates.NOT_FOLDING;
+            mLastState = lastState;
+            mPaint2.setColor(mPaint3.getColor());
+            mPaint2.setAlpha(OPAQUE);
+
+        }
+
+        return axisValue+mSpeed;
+    }
+
+    private int foldingToMinimun(int axisValue, ProgressStates lastState, int nextColor) {
+
+        //Start
+        if (axisValue >= mContorlPointMaximun) {
+            axisValue = mContorlPointMaximun;
+            mPaint3.setAlpha(FOLDED_TRANSPARENCY);
+        }
+
+        //Half
+        if (axisValue <= mHalf) {
+            mPaint3.setColor(nextColor);
+            mPaint3.setAlpha(FOLDED_TRANSPARENCY);
+        }
+
+        //End
+        if (axisValue <= mControlPointMinimun) {
+            axisValue = mControlPointMinimun;
+            mCurrentState = ProgressStates.NOT_FOLDING;
+            mLastState = lastState;
+            mPaint.setColor(mPaint3.getColor());
+            mPaint.setAlpha(OPAQUE);
+        }
+
+        return axisValue - mSpeed;
+    }
+
+    private void drawXMotion(Canvas canvas) {
+        canvas.drawArc(mOval, 90, 180, true, mPaint);
+        canvas.drawArc(mOval, -270, -180, true, mPaint2);
+        mPath.reset();
+        mPath.moveTo(mHalf, 0);
+        mPath.cubicTo(mX, 0, mX, mDiameter, mHalf, mDiameter);
+    }
+
+    private void drawYMotion(Canvas canvas) {
+        canvas.drawArc(mOval, 0, -180, true, mPaint);
+        canvas.drawArc(mOval, -180, -180, true, mPaint2);
+        mPath.reset();
+        mPath.moveTo(0, mHalf);
+        mPath.cubicTo(0, mY, mDiameter, mY, mDiameter, mHalf);
     }
 }
