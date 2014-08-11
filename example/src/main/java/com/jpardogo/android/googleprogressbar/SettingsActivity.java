8947b9f1d5ec12df0b9f10ad77bc514366388bc6
jpardogo <jpardogo@gmail.com>
2014-08-11 18:56:48 +0200

    Navigation Up from Settings in the Example app

--- example/src/main/java/com/jpardogo/android/googleprogressbar/SettingsActivity.java
+++ example/src/main/java/com/jpardogo/android/googleprogressbar/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 /**
  * Created by jpardogo on 08/08/2014.
@@ -19,5 +21,16 @@ public class SettingsActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
